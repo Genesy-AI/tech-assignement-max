27630d24a868a9afc932c54f91ad008d141b8a87
Max De La Rosa Toro <max@swipoo.com>
2025-09-19 17:10:53 +0200

    feat: add PhoneNumberProvider interface and create a class for each provider

---
+++ backend/src/workflows/activities/findPhoneNumber/phoneNumberProviders/AstraDialerPhoneNumberProvider.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { envVariables } from '../../../../config/envVariables'
+import { FindPhoneNumberInputData, PhoneNumberProvider } from './PhoneNumberProvider'
+import z from 'zod'
+
+const astraDialerRequestBodySchema = z.object({ email: z.email() })
+
+type AstraDialerResponse = { phoneNmbr: string | null | undefined }
+
+export class AstraDialerPhoneNumberProvider implements PhoneNumberProvider {
+  baseUrl: string
+
+  constructor() {
+    this.baseUrl = 'https://api.genesy.ai/api/tmp/astraDialer'
+  }
+
+  async findPhone(inputData: FindPhoneNumberInputData): Promise<string | null> {
+    const payload = astraDialerRequestBodySchema.parse(inputData)
+
+    const { data } = await axios.post<AstraDialerResponse>(this.baseUrl, payload, {
+      headers: {
+        apiKey: envVariables.ASTRA_DIALER_API_KEY,
+      },
+    })
+
+    return data.phoneNmbr ?? null
+  }
+}
---
+++ backend/src/workflows/activities/findPhoneNumber/phoneNumberProviders/NimbusLookupPhoneNumberProvider.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { FindPhoneNumberInputData, PhoneNumberProvider } from './PhoneNumberProvider'
+import { envVariables } from '../../../../config/envVariables'
+import z from 'zod'
+
+const nimbusRequestBodySchema = z.object({ email: z.email(), jobTitle: z.string() })
+
+type NimbusLookupResponse = { number: number; countryCode: 'string' }
+
+export class NimbusLookupPhoneNumberProvider implements PhoneNumberProvider {
+  baseUrl: string
+
+  constructor() {
+    this.baseUrl = 'https://api.genesy.ai/api/tmp/nimbusLookup'
+  }
+
+  async findPhone(inputData: FindPhoneNumberInputData): Promise<string | null> {
+    const payload = nimbusRequestBodySchema.parse(inputData)
+
+    const { data } = await axios.post<NimbusLookupResponse>(
+      `${this.baseUrl}?api=${envVariables.NIMBUS_LOOKUP_API_KEY}`,
+      payload
+    )
+
+    return `${data.countryCode}${data.number}`
+  }
+}
