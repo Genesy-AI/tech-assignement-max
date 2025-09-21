9d6b44b9a136e0631013b8a64478125d6b840a12
Max De La Rosa Toro <max@swipoo.com>
2025-09-21 15:57:11 +0200

    refactor: create ClockIcon component

---
+++ frontend/src/icons/ClockIcon.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react'
+
+export const ClockIcon: FC = () => (
+  <svg
+    className="mr-3 h-4 w-4"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M16 12H8m8 0a8 8 0 11-16 0 8 8 0 0116 0zm-8 0V4"
+    />
+  </svg>
+)
